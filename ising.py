e674802b39135b85d356a600086cf79d15c57a9a
YifanFei <ivanfee@outlook.com>
2022-04-17 12:24:49 +0800

    Usable script

--- ising.py
+++ ising.py
@@ -1,35 +1,32 @@
-
 from __future__ import division
 import numpy as np
 
-def init_lattice(n):
 
-    '''Create a nxn lattice with random spin configuration'''
-    
+def init_lattice(n):
+    """Create a nxn lattice with random spin configuration"""
+
     lattice = np.random.choice([1, -1], size=(n, n))
     return lattice
 
 
 def deltaE(S0, Sn, J, H):
+    """Energy difference for a spin flip"""
 
-    '''Energy difference for a spin flip'''
-    
     return 2 * S0 * (H + J * Sn)
 
 
-def ising(n=200, 
+def ising(n=200,
           nsteps=500000,
           H=0,
           J=1,
           T=1,
-          count_spins = False,
-          countij = [1,1],
+          count_spins=False,
+          countij=[1, 1],
           correlation=False,
-          corr_ij=[0,0],
-          corr_r=1):
-
+          corr_ij=[0, 0],
+          corr_r=1, xrange=None):
     '''Ising Model Simulator. If count_spins = True, only flipping behavior of 1 site is studied.'''
-    
+
     lattice = init_lattice(n)
     energy = 0
     energies = []
@@ -41,8 +38,8 @@ def ising(n=200,
     icorr, jcorr = corr_ij
     Sis = []
     SiSjs = []
-    
-    for step in xrange(nsteps):
+
+    for step in range(nsteps):
 
         i = np.random.randint(n)
         j = np.random.randint(n)
@@ -53,13 +50,13 @@ def ising(n=200,
 
         dE = deltaE(lattice[i, j], Sn, J, H)
 
-        if dE < 0 or np.random.random() < np.exp(-dE/T):
+        if dE < 0 or np.random.random() < np.exp(-dE / T):
             lattice[i, j] = -lattice[i, j]
             energy += dE
             energies.append(energy)
-         # Note that the spin is collected at every step
-            spin += 2*lattice[i, j]
-        
+            # Note that the spin is collected at every step
+            spin += 2 * lattice[i, j]
+
         if count_spins:
             ispin = lattice[icount, jcount]
             if ispin != counted_spins[-1]:
@@ -74,32 +71,30 @@ def ising(n=200,
             SiSj = Si * Sn_corr / 4.0
             Sis.append(Si)
             SiSjs.append(SiSj)
-            
-        spins.append(spin)
 
+        spins.append(spin)
 
     if correlation:
         return Sis, SiSjs
-    
+
     if count_spins:
         return counted_spins, counted_intervals
-        
+
     return lattice, energies, spins
-    
+
 
 def ising1000(n=1000, nsteps=10000000000, H=0, J=1, T=1):
-
     '''Ising Model Simulator. Special case for very large lattices.
     To reduce memory usage,
     Spin is added to the array every 1000 steps.
     Energies are not returned.'''
-    
+
     lattice = init_lattice(n)
     energy = 0
 
     spins = []
     spin = np.sum(lattice)
-    for istep, step in enumerate(xrange(nsteps)):
+    for istep, step in enumerate(range(nsteps)):
 
         i = np.random.randint(n)
         j = np.random.randint(n)
@@ -110,37 +105,37 @@ def ising1000(n=1000, nsteps=10000000000, H=0, J=1, T=1):
 
         dE = deltaE(lattice[i, j], Sn, J, H)
 
-        if dE < 0 or np.random.random() < np.exp(-dE/T):
+        if dE < 0 or np.random.random() < np.exp(-dE / T):
             lattice[i, j] = -lattice[i, j]
             energy += dE
-            spin += 2*lattice[i, j]
+            spin += 2 * lattice[i, j]
         if istep % 1000 == 0:
             spins.append(spin)
     return lattice, spins
 
 
-
-def write_job_script(wd='./', n=10, s=1000, i=1, T=1., nprocs=1, pe='smp', name = 'batch', q = 'long'):
+def write_job_script(wd='./', n=10, s=1000, i=1, T=1., nprocs=1, pe='smp', name='batch', q='long'):
     '''
     This is a function that writes a script to submit MC jobs
     '''
     py_file = '/afs/crc.nd.edu/user/p/pmehta1/ising-monte-carlo/spins.py'
-    script='''#!/bin/bash
+    script = '''#!/bin/bash
 #$ -N {0}
 #$ -pe {1} {2}
 #$ -q {3}
 #$ -cwd
 '''.format(name, pe, nprocs, q)
-       
+
     if nprocs > 1:
         cmd = 'mpirun -np $NSLOTS python'
-        script+='{6} {5} -n {0} -s {1} -i {2} -t {3} -w {4}'.format(n, s, i, T, wd, py_file, cmd)
+        script += '{6} {5} -n {0} -s {1} -i {2} -t {3} -w {4}'.format(n, s, i, T, wd, py_file, cmd)
 
     else:
-        script+='python {5} -n {0} -s {1} -i {2} -t {3} -w {4}'.format(n, s, i, T, wd, py_file)
+        script += 'python {5} -n {0} -s {1} -i {2} -t {3} -w {4}'.format(n, s, i, T, wd, py_file)
 
     with open('{0}/qscript'.format(wd), 'w') as f:
         f.write(script)
+
 
 def run_job(wd):
     '''
@@ -152,13 +147,13 @@ def run_job(wd):
     os.chdir(wd)
     p = Popen(['qsub', 'qscript'], stdout=PIPE, stderr=PIPE)
     out, err = p.communicate()
-    
-    if out == '' or err !='':
+
+    if out == '' or err != '':
         raise Exception('something went wrong in qsub:\n\n{0}'.format(err))
     jobid = out.split()[2]
     f = open('jobid', 'w')
-  
+
     f.write(jobid)
     f.close()
     os.chdir(cwd)
-    return out.strip()    
+    return out.strip()
